b112a7eb79b22c0f69ca3f363a45b25fa8bd05e6
Xpol Wan <xpolife@gmail.com>
2023-07-14 12:29:43 +0800

    Use shared library within workspace

--- apps/my-app-with-import/src/my-app-with-import.module.ts
+++ apps/my-app-with-import/src/my-app-with-import.module.ts
@@ -1,9 +1,10 @@
+import { SharedModule } from "@app/shared";
 import { Module } from '@nestjs/common';
 import { MyAppWithImportController } from './my-app-with-import.controller';
 import { MyAppWithImportService } from './my-app-with-import.service';
 
 @Module({
-  imports: [],
+  imports: [SharedModule],
   controllers: [MyAppWithImportController],
   providers: [MyAppWithImportService],
 })
